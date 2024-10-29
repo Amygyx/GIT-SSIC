62c403736fb861e572969224cb149293aee2d135
RuoyuFeng <ustcfry@mail.ustc.edu.cn>
2024-10-29 03:56:59 +0000

    Clean Code.

--- debug.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-print(torch.__version__) 
-print(hasattr(torch, 'argwhere'))  # 应输出 True
--- debug.sh
+++
@@ -1,8 +0,0 @@
-# Train GIT-SSIC model.
-CUDA_VISIBLE_DEVICES=0 python train.py \
-    --model ours_groupswin_channelar --hyper-channels 192 192 192 \
-    --lmbda 8192 --lr 5e-5 \
-    --train-set /home/t2vg-a100-G4-10/project/qyp/datasets/COCO/train2017 \
-    --eval-set /home/t2vg-a100-G4-10/mnt/guangtingsc_fengry/dataset/CompressionData/kodak \
-    --groupvit-load-group-msk figs/kodak/group_msk \
-    --total-iteration 2000000 --multistep-milestones 1600000 --eval-interval 10
